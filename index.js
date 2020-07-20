095890cf0c4c9255b4194cdce8ab03da5fdbe40e
amelexik <amelexik@gmail.com>
2020-07-20 11:34:48 +0300

    beta

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config({path: __dirname + '/.env'});
 const express = require('express')
-const userRouter = require('./src/routes')
 const port = process.env.PORT  || 5000;
+const userRouter = require('./src/routes')
+
 require('./src/db')
 
 const app = express()
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const User = require('./models/User')
-
 const router = express.Router()
+const path = require('path')
+const directoryPath = path.join(__dirname, '..', process.env.SEARCH_DIR);
 
 router.post('/users', async (req, res) => {
     // Create a new user
@@ -9,26 +10,45 @@ router.post('/users', async (req, res) => {
         const user = new User(req.body)
         await user.save()
         const token = await user.generateAuthToken()
-        res.status(201).send({ user, token })
+        res.status(201).send({user, token})
     } catch (error) {
         res.status(400).send(error)
     }
 })
 
-router.post('/users/login', async(req, res) => {
+router.post('/users/login', async (req, res) => {
     //Login a registered user
     try {
-        const { email, password } = req.body
+        const {email, password} = req.body
         const user = await User.findByCredentials(email, password)
         if (!user) {
             return res.status(401).send({error: 'Login failed! Check authentication credentials'})
         }
         const token = await user.generateAuthToken()
-        res.send({ user, token })
+        res.send({user, token})
     } catch (error) {
         res.status(400).send(error)
     }
+})
 
+router.get("/", (req, res, next) => {
+    res.send('Go to <a href="/search/lorem">/search/:term</a>')
+})
+
+router.get("/search/:term", (req, res, next) => {
+
+    /**
+     * todo необходимо сделать авторизацию пользователя используя JWT
+     */
+
+    let term = req.params.term;
+    let searchInFile = require("search-in-file");
+    let responseObject = {'term':term,'result':null};
+    searchInFile.fileSearch([directoryPath], term, {recursive: true, searchResults: 'lineNo'})
+        .then(function (results) {
+            responseObject.result = results;
+            res.json(responseObject);
+        });
 })
 
 module.exports = router
