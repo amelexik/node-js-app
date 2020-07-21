2b06a843b10556eefa17b99e775c903cbc417f5b
amelexik <amelexik@gmail.com>
2020-07-21 08:39:43 +0300

    processing

--- index.js
+++ index.js
@@ -1,45 +1,14 @@
-// wiki.js - Wiki route module.
+require('dotenv').config({path: __dirname + '/.env'});
+const express = require('express')
+const userRouter = require('./src/routes')
+const port = process.env.PORT  || 5000;
+require('./src/db')
 
-const express = require('express');
-const app = express();
+const app = express()
 
-//requiring path and fs modules
-//joining path of directory
+app.use(express.json())
+app.use(userRouter)
 
-//passsing directoryPath and callback function
-
-
-let files = new Object();
-
-
-//https://stackoverflow.com/questions/5827612/node-js-fs-readdir-recursive-directory-search
-
-let fs = require('fs');
-let path = require('path');
-
-const directoryPath = path.join(__dirname, 'storage');
-
-
-let searchInFile = require("search-in-file");
-
-
-app.get("/", (req, res, next) => {
-    searchInFile.fileSearch([directoryPath], 'php', {recursive: true,searchResults:'lineNo'})
-        .then(function (results) {
-            res.json(results);
-        });
-});
-
-app.get('/search/:term', function (req, res) {
-    let term = req.params.term;
-    let response = {
-        'term' : term
-    }
-    res.json(response)
-});
-
-
-let port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
